a400f31aa19ad306f1ea968d52c93f1d860c70b6
DebaA17 <debashis.2520017@bppimt.com>
2025-08-13 21:59:47 +0530

    added tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,18 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src=\"https://www.clarity.ms/tag/\"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, \"clarity\", \"script\", \"su9vtu8m80\");
+              `,
+            }}
+          />
       </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
